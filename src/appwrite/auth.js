b130559ebc933b6e6f622dabdbd16af3a8b90ce3
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-07-30 02:39:28 +0530

    Appwrite authentication

---
+++ src/appwrite/auth.js
@@ -0,0 +1,52 @@
+import conf from '../conf/conf.js'
+import { Client, Account, ID } from 'appwrite'
+
+export class AuthService{
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+    async createAccount ({email, password, name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if(userAccount){
+                //call another method
+                return this.login({email, password});
+            }
+            else{
+                return userAccount;
+            }
+        }
+        catch(error){
+            console.log("Appwrite :: createAccount :: Error", error);;
+        }
+    }
+    async login({email, password}){
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            console.log("Appwrite :: Login :: Error", error);;
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite :: currentUser :: Error", error);;
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite :: Logout :: Error", error);
+        }
+    }
+}
+const authService = new AuthService();
+export default authService
