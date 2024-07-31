257b95ae3b5c391a29120af3bdaf2d403a0b9976
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-08-01 02:06:09 +0530

    listPost

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -74,6 +74,18 @@ export class Service{
             return false;
         }
     }
+    async listPost(queries = [Query.equal("status","active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: listPost :: error", error);
+            return false;
+        }
+    }
 }
 
 const service = new Service()
