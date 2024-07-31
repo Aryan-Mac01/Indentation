8c39684da376a9288cca24b60698db801dc4f7a6
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-08-01 01:50:40 +0530

    createPost

---
+++ src/appwrite/config.js
@@ -0,0 +1,38 @@
+import conf from "../conf/conf";
+import { Client, ID, Databases, Query, Storage } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    storage;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content, 
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service : createPost :: Error", error);
+        }
+    }
+}
+
+const service = new Service()
+export default service
