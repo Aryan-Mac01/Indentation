26ac3687528b2f91946fe041e046a8a8c9f9295f
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-08-12 12:17:07 +0530

    UL

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,44 +10,61 @@ function Header() {
 
   const navItems = [
     {
-      name: 'Home',
-      slug: '/',
-      active: true
-    }, 
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
     {
-      name: 'Login',
-      slug: '/login',
+      name: "Login",
+      slug: "/login",
       active: !authStatus,
     },
     {
-      name: 'Signup',
-      slug: '/signup',
+      name: "Signup",
+      slug: "/signup",
       active: !authStatus,
     },
     {
       name: "All-Posts",
-      slug: '/all-posts',
+      slug: "/all-posts",
       active: authStatus,
     },
     {
       name: "Add-Posts",
-      slug: '/add-posts',
+      slug: "/add-posts",
       active: authStatus,
-    }
-  ]  
-  return(
-     <header className="py-3 shadow bg-gray-500">
+    },
+  ];
+  return (
+    <header className="py-3 shadow bg-gray-500">
       <Container>
         <nav className="flex">
           <div className="mr-4">
-              <Link to='/'>
-                <Logo width="70px"/>
-              </Link>
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
           </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  ></button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn/>
+              </li>
+            )}
+          </ul>
         </nav>
       </Container>
-     </header>
-  )
+    </header>
+  );
 }
 
 export default Header;
