24e93753cf40156a1f44e99d20356d40078b280b
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-08-12 11:32:31 +0530

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,47 @@ import { useNavigate } from "react-router-dom";
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: true
+    }, 
+    {
+      name: 'Login',
+      slug: '/login',
+      active: !authStatus,
+    },
+    {
+      name: 'Signup',
+      slug: '/signup',
+      active: !authStatus,
+    },
+    {
+      name: "All-Posts",
+      slug: '/all-posts',
+      active: authStatus,
+    },
+    {
+      name: "Add-Posts",
+      slug: '/add-posts',
+      active: authStatus,
+    }
+  ]  
   return(
-     <div>Header</div>
+     <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+              <Link to='/'>
+                <Logo width="70px"/>
+              </Link>
+          </div>
+        </nav>
+      </Container>
+     </header>
   )
 }
 
