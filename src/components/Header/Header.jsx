f83c8be512129b247ca3f5bd2f7f8787b05caa80
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-08-12 11:22:43 +0530

    authStatus

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
-  return (
-    <div>Header</div>
+  const authStatus = useSelector((state) => state.auth.status);
+  return(
+     <div>Header</div>
   )
 }
 
-export default Header
+export default Header;
--- src/components/Header/logoutBtn.jsx
+++ src/components/Header/logoutBtn.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import authService from '../../appwrite/config'
-import {logout} from '../../store/authSlice'
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/config";
+import { logout } from "../../store/authSlice";
 
-function logoutBtn() {
-    const dispatch = useDispatch()
-    const handleLogout = () => {
-        authService.logout().then(() => {
-            dispatch(logout())
-        })
-        .catch(
-            console.log('error')
-        )
-    }
+function LogoutBtn() {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    authService
+      .logout()
+      .then(() => {
+        dispatch(logout());
+      })
+      .catch(console.log("error"));
+  };
   return (
-    <div>logoutBtn</div>
-  )
+    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+      Logout
+    </button>
+  );
 }
 
-export default logoutBtn
+export default LogoutBtn;
