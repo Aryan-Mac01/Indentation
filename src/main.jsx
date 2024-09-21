eb531275051f39b533c07042d2414d51213263f3
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-09-22 02:27:07 +0530

    routing initial

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,20 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children:[
+      {
+        path: '/',
+        element:<Home/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
